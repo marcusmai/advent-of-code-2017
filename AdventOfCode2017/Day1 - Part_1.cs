99047678d16accaa41a4785618f89dcf0e111b30
marcusm83 <34194070+marcusm83@users.noreply.github.com>
2017-12-02 19:37:47 +0100

    Day 1

---
+++ AdventOfCode2017/Day1 - Part_1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = System.IO.File.ReadAllText(@"C:\AdventOfCode2017\input_01.txt");
+            int sum = 0;
+             
+            for (int i = 0; i <= (input.Length-1); i++)
+            {
+                int currentChar = i;
+                int nextChar = i+1;
+     
+                if (i != input.Length - 1)
+                {
+                    if (input[currentChar] == input[nextChar])
+                    {
+                        sum = sum + (int)Char.GetNumericValue(input[currentChar]);
+                    }
+                }
+                else
+                {
+                    if (input[currentChar] == input[0])
+                    {
+                        sum = sum + (int)Char.GetNumericValue(input[currentChar]);
+                    }
+                }
+            }
+            
+            Console.WriteLine("Resultat: {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdventOfCode2017/Day1 - Part_2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = System.IO.File.ReadAllText(@"C:\AdventOfCode2017\input_01.txt");
+            int sum = 0;
+            int nextChar = (input.Length / 2) -1;
+
+            for (int i = 0; i <= (input.Length - 1); i++)
+            {
+                int currentChar = i;
+                nextChar = nextChar + 1;
+
+                if (i != input.Length)
+                {
+                    if (input[currentChar] == input[nextChar])
+                    {
+                        sum = sum + (int)Char.GetNumericValue(input[currentChar]);
+                    }
+                    if (nextChar == input.Length-1)
+                    {
+                        nextChar = -1;
+                    }
+                }
+            }
+
+            Console.WriteLine("Resultat: {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
