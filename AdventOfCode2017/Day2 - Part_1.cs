0445c2cdc6ecf57f6f463675c34b68db9cfb49d4
marcusm83 <34194070+marcusm83@users.noreply.github.com>
2017-12-02 19:37:56 +0100

    Day 2

---
+++ AdventOfCode2017/Day2 - Part_1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string line;
+            System.IO.StreamReader input = new System.IO.StreamReader(@"C:\AdventOfCode2017\input_02.txt");
+            int sum = 0;
+
+            while ((line = input.ReadLine()) != null)
+            {
+                int highVal = 0;
+                int lowVal = 9999;
+                String[] lineVal = line.Split('\t');
+
+                for (int i = 0; i < lineVal.Length; i++)
+                {
+                    int value = Int32.Parse(lineVal[i]);
+
+                    if (value > highVal)
+                    {
+                        highVal = value;
+                    }
+                    if (value < lowVal)
+                    {
+                        lowVal = value;
+                    }
+                }
+                sum = sum + (highVal - lowVal);
+            }
+
+            Console.WriteLine("Resultat: {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdventOfCode2017/Day2 - Part_2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            System.IO.StreamReader input = new System.IO.StreamReader(@"C:\AdventOfCode2017\input_02.txt");
+            int sum = 0;
+            int delsum = 0;
+
+            while ((line = input.ReadLine()) != null)
+            {
+                String[] lineVal = line.Split('\t');
+
+                for (int i = 0; i < lineVal.Length; i++)
+                {
+                    int value1 = Int32.Parse(lineVal[i]);
+                    
+                    for (int y = (i+1); y < lineVal.Length ; y++)
+                    {
+                        int value2 = Int32.Parse(lineVal[y]);
+                        float divVal = (float)value1 / (float)value2;
+                        if ((divVal % 1) == 0)
+                        {
+                            delsum = (int)divVal;
+                        }
+                    }
+                   for (int x = 0; x < i; x++)
+                    {
+                        int value2 = Int32.Parse(lineVal[x]);
+                        float divVal = (float)value1 / (float)value2;
+                        if ((divVal % 1) == 0)
+                        {
+                            delsum = (int)divVal;
+                        }
+                    }
+                }
+                sum = sum + delsum;
+            }
+
+            Console.WriteLine("Resultat: {0}", sum);
+            Console.ReadKey();
+            
+        }
+    }
+}
