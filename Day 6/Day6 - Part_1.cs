3f6b4a3ba96664440ef97e758a2b73cc6673f085
marcusm83 <34194070+marcusm83@users.noreply.github.com>
2017-12-06 21:35:51 +0100

    Day 6

---
+++ Day 6/Day6 - Part_1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AdventOfCode.Day6
+{
+    class Day6
+    {
+        static void Main(string[] args)
+        {
+            List<string> memoryStates = new List<string>();
+            string memoryString = "";
+            int cycles = 0;
+            string[] input = File.ReadAllText("Day6\\input.txt").Split('\t').ToArray();
+            int[] memory = new int[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+                memory[i] = Int32.Parse(input[i]);
+
+            for (int i = 0; i < memory.Length; i++) {memoryString = memoryString + memory[i].ToString();}
+            memoryStates.Add(memoryString);
+            memoryString = "";
+
+            while (true)
+            {
+                int startIndex = memory.ToList().IndexOf(memory.Max());
+                int blocks = memory[startIndex];
+                memory[startIndex] = 0;
+
+                for (int i = 0; i < blocks; i++)
+                {
+                    if (startIndex >= memory.Length - 1)
+                        startIndex = -1;
+
+                    memory[startIndex + 1] += 1;
+                    startIndex++;
+                }
+
+                for (int i = 0; i < memory.Length; i++) { memoryString = memoryString + memory[i].ToString(); }
+                
+                cycles++;
+
+                if (memoryStates.Contains(memoryString))
+                {
+                    break;
+                }
+
+                memoryStates.Add(memoryString);
+                memoryString = "";
+
+            }
+
+            Console.WriteLine("Result: {0}", cycles);
+            Console.ReadKey();
+        }
+    }
+}
